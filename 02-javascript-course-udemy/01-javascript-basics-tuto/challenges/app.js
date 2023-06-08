2f8b8e42ddb2dea5833390e1f707df55b2db4a35
Marc <marcguybretones@gmail.com>
2023-06-08 11:49:46 -0400

    continue js basics

---
+++ 02-javascript-course-udemy/01-javascript-basics-tuto/challenges/app.js
@@ -0,0 +1,7 @@
+const firstName = "Marc";
+const lastName = "Bretones";
+let address = "main street";
+
+address = "first street";
+
+console.log(firstName, lastName, address);
--- 02-javascript-course-udemy/01-javascript-basics-tuto/external/app.js
+++ 02-javascript-course-udemy/01-javascript-basics-tuto/external/app.js
@@ -6,5 +6,32 @@ alert("Hello World");
 console.log("Hello World");
 */
 
-// VARIABLES - basic block; classic store, access, modify === value; declare, assignment operator, assign value
-//
+// VARIABLES - basic block; classic store, access, modify === value; declare, assignment operator, assign value;
+// Undefined != error.
+
+/*let myName = "John Doe"; //Easy to change everything in one place.
+console.log(myName);
+// console.log(name);
+// console.log(name);
+
+let address, zip, state;
+address = "100 Main St";
+console.log(address);
+zip = "12345";
+console.log(zip);
+state = "FL";
+console.log(state);
+
+myName = "Steve Smith";
+console.log(myName);
+
+console.log(address, zip, state);*/
+
+// Var names are: case sensitive (fullName != FullName), can't start with number, can't have spaces or hyphens, use camelCase. Can't use reserved keywords (ie. let).
+
+// CONST & VAR - const = constant, var = variable; const = can't be reassigned, var = can be reassigned (old way of creating variables).
+//VAR - don't use anymore really - use let and const instead.
+/*
+const lastName = "Doe";
+//lastName = "Smith"; //Can't reassign.
+console.log(lastName); //Throws an error.*/
