579a092e58564dcab4270e20ae55c8046806b1a7
Marc <marcguybretones@gmail.com>
2023-06-08 00:41:30 -0400

    start js course

---
+++ 02-javascript-course-udemy/01-javascript-basics-tuto/external/app.js
@@ -0,0 +1,10 @@
+// HELPER METHODS
+
+/*
+document.write("Hello World");
+alert("Hello World");
+console.log("Hello World");
+*/
+
+// VARIABLES - basic block; classic store, access, modify === value; declare, assignment operator, assign value
+//
